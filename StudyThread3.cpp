1d23465d4a45728b95a8f5fbd8c7f59b6158415e
BATMINJI <minjiconan@naver.com>
2026-02-26 13:50:17 +0900

    뮤텍스를 써도, 데드락이 발생하는 경우이다.

---
+++ StudyThread3.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+
+void Worker1(std::mutex& m1, std::mutex& m2)
+{
+	for (int i = 0; i < 10000; ++i)
+	{
+		std::lock_guard<std::mutex> Lock1(m1);
+		std::lock_guard<std::mutex> Lock2(m2);
+	}
+}
+
+void Worker2(std::mutex& m1, std::mutex& m2)
+{
+	for (int i = 0; i < 10000; ++i)
+	{
+		std::lock_guard<std::mutex> Lock2(m2);
+		std::lock_guard<std::mutex> Lock1(m1);
+	}
+}
+
+int main()
+{
+	int Counter = 0;
+	std::mutex m1;
+	std::mutex m2;
+
+	std::thread t1(Worker1, std::ref(m1), std::ref(m2));
+	std::thread t2(Worker2, std::ref(m1), std::ref(m2));
+
+	t1.join();
+	t2.join();
+
+	std::cout << "³¡" << std::endl;
+}
